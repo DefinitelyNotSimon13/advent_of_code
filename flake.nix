f39e217df2ac663f7e4e0c305d03136823565dd1
Simon <simon21.blum@gmail.com>
2024-12-01 16:13:10 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            bacon
+            clippy
+            cargo
+            rustc
+            rust-analyzer
+            cargo-watch
+          ];
+
+          buildInputs =
+            with pkgs;
+            [
+            ];
+
+        };
+      }
+    );
+}
